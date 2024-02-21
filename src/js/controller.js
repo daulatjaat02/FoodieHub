4c09cd8d891a0aaf71c0e19068a8cb16d25e7dcb
daulat <daulatjajra2006@gmail.com>
2024-02-21 16:26:21 +0530

    recipe

--- src/js/controller.js
+++ src/js/controller.js
@@ -29,11 +29,16 @@ const renderSpinner = function (parentEl) {
 };
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return;
+
     // 1) Loading recipe
     renderSpinner(recipeContainer);
     const res = await fetch(
       // `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
-      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bca36`
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
 
@@ -153,7 +158,11 @@ const showRecipe = async function () {
     recipeContainer.innerHTML = "";
     recipeContainer.insertAdjacentHTML("afterbegin", markup);
   } catch (err) {
-    alert(err);
+    console.log(err);
   }
 };
-showRecipe();
+
+// window.addEventListener("hashchange", showRecipe);
+// window.addEventListener("load", showRecipe);
+
+["hashchange", "load"].forEach((ev) => window.addEventListener(ev, showRecipe));
